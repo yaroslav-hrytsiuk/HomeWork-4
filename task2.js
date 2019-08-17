604e7a9db2655ccdbd0b85702189f9bd37468e03
slavik3333 <slavik3333@yahoo.com>
2019-08-17 18:02:37 +0300

    4 task finish

--- task2.js
+++ task2.js
@@ -20,4 +20,4 @@ function argLength(arr) {
         return ('Sum of arguments - ' + argSum);
     }
 }
-console.log(argLength(1, 2));
+console.log(argLength(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
--- task4.js
+++ task4.js
@@ -3,20 +3,36 @@
 console.log('\n');
 console.log('Task 4');
 
-
-
+// function sortArray(arr) {
+//     for(let counter = arr.length; counter > 0; counter-- ) {
+//         for (let i = 1; i < arr.length; i++) {
+//             if (arr[i] < arr[i - 1]) {
+//                 let tmp = arr[i];
+//                 arr[i] = arr[i - 1];
+//                 arr[i - 1] = tmp;
+//                 i = 0;
+//             }
+//         }
+//     }
+//     return arr;
+// }
 function sortArray(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < arr[i - 1]) {
-            let tmp = arr[i];
-            arr[i] = arr[i - 1];
-            arr[i - 1] = tmp;
-            i = 0;
+        let counter = true;
+        while (counter) {
+            counter = false;
+            for (let i = 1; i < arr.length; i++) {
+                if (arr[i] < arr[i-1]) {
+                    let tmp = arr[i];
+                    arr[i] = arr[i - 1];
+                    arr[i - 1] = tmp;
+                    counter = true;
+                }
+            }
         }
+        return arr;
     }
-    return arr;
-}
 
 const array = [25, 1, 99, 36, 41, 9, 15, 3, 56, 55];
 console.log(array);
 console.log(sortArray(array));
+
