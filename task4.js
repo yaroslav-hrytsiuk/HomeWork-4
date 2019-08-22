cbde52f2044f69603ee53474bdf744ce058a21af
slavik3333 <slavik3333@yahoo.com>
2019-08-22 09:20:24 +0300

    Ready insertion sort

--- task4.js
+++ task4.js
@@ -3,19 +3,6 @@
 console.log('\n');
 console.log('Task 4');
 
-// function sortArray(arr) {
-//     for(let counter = arr.length; counter > 0; counter-- ) {
-//         for (let i = 1; i < arr.length; i++) {
-//             if (arr[i] < arr[i - 1]) {
-//                 let tmp = arr[i];
-//                 arr[i] = arr[i - 1];
-//                 arr[i - 1] = tmp;
-//                 i = 0;
-//             }
-//         }
-//     }
-//     return arr;
-// }
 function bubbleSort(arr) {
     let counter = true;
     while (counter) {
@@ -37,22 +24,19 @@ console.log(array);
 console.log(bubbleSort(array));
 console.log('\n');
 
-// function insertionSort(arr) {
-//     let counter = true;
-//     while (counter) {
-//         counter = false;
-//         for (let i = arr.length; i > 0; i--) {
-//             if (arr[i] < arr[i-1]) {
-//                 let tmp = arr[i];
-//                 arr[i] = arr[i - 1];
-//                 arr[i - 1] = tmp;
-//                 counter = true;
-//             }
-//         }
-//     }
-//     return arr;
-// }
-// const arrayTwo = [25, 1, 99, 36, 41, 9, 15, 3, 56, 55];
-// console.log(arrayTwo);
-// console.log(insertionSort(arrayTwo));
+function insertionSort(arr) {
+    for (let counter = 0; counter < arr.length; counter++) {
+        let i = counter - 1;
+        let el = arr[counter];
+        while(i >= 0 && arr[i] > el) {
+            arr[i+1] = arr[i];
+            i--;
+        }
+        arr[i+1] = el;
+    }
+    return arr;
+}
+const arrayTwo = [25, 1, 99, 36, 41, 9, 15, 3, 56, 55];
+console.log(arrayTwo);
+console.log(insertionSort(arrayTwo));
 
